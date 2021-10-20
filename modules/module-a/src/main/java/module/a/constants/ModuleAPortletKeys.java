2a21f56cc6b9e125f2f4a39a163d000002cf8579
krishnar97 <krishna.rajappa@liferay.com>
2021-10-20 10:20:16 +0530

    Initial modules

---
+++ modules/module-a/src/main/java/module/a/constants/ModuleAPortletKeys.java
@@ -0,0 +1,11 @@
+package module.a.constants;
+
+/**
+ * @author Liferay
+ */
+public class ModuleAPortletKeys {
+
+	public static final String MODULEA =
+		"module_a_ModuleAPortlet";
+
+}
---
+++ modules/module-b/src/main/java/module/b/constants/ModuleBPortletKeys.java
@@ -0,0 +1,11 @@
+package module.b.constants;
+
+/**
+ * @author Liferay
+ */
+public class ModuleBPortletKeys {
+
+	public static final String MODULEB =
+		"module_b_ModuleBPortlet";
+
+}
